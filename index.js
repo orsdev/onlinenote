4e8d602cc450c2c06ec4279018c9d8df601bf72e
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-03 15:52:35 +0100

    ajax call created for login modal

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
 
+ //apply event to signup form
  $('#formsignup').submit(function(e) {
   //prevent form default action
   e.preventDefault();
-  //collect and store users inputs in varibles
+  //collect and store users inputs
   let dataPost = $(this).serializeArray();
 
   //send them to signup.php using AJAX
@@ -22,6 +23,36 @@ $(document).ready(function () {
    }
   });
 
+ 
+
+});
+
+ 
+//apply event to login form
+$('#formlogin').submit(function(e){
+ //prevent form default action
+ e.preventDefault();
+ //collect and store users inputs
+ let dataPost = $(this).serializeArray();
+
+ //send them to login.php using AJAX
+ $.ajax({
+  type: "POST",
+  url: "login.php",
+  data: dataPost,
+  success: function(response) { 
+    if(response == 'success') {
+     window.location = 'mainpage.php';
+    }else {
+    $('.login-message').html(response);
+    }
+  },
+  error: function(error){
+   let err = '<php class="alert alert-danger"> Something went wrong, Try again. </php>';
+   $('.login-message').html(err);
+  }
  });
+
+});
 
 });
