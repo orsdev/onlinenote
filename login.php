52b785bd733f89ab24b31a7a0aa189dbed2cd4ec
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-03 17:04:00 +0100

    added detailed comments to login.php

--- login.php
+++ login.php
@@ -72,21 +72,39 @@ if ($rowCount == 1) {
   generate two random numbers
    and store in two variables
   */
+
+   /*
+   generate reandom strings and convert to hexadecimal,
+   10bytes = 10 * 8(1 bytes = 8bits) => 80 => 80 / 4 => 20characters
+  */
   $authentificator1 = bin2hex(openssl_random_pseudo_bytes(10));
-  $authentificator2 = openssl_random_pseudo_bytes(20);
+  /*
+   generate reandom strings,
+   20bytes = 20 * 8(1 bytes = 8bits) => 160 => 160 / 4 => 40characters
+  */
+  $authentificator2 = openssl_random_pseudo_bytes(20); 
 
   //store in a cookie
 
-  //concat parameters
+  /*
+  concat function parameter a & b,
+  convert $b to hexadecimal
+  
+  ** $authentificator 2 is only converted
+  to hexadecimal in auth function, outside 
+  of the function, it is random strings
+  */
   function auth($a, $b)
   {
    $c = $a . "," . bin2hex($b);
    return $c;
   };
 
+  //store cookie name and value in a variable
   $cookieName = 'rememberme';
   $cookieValue = auth($authentificator1, $authentificator2);
 
+  //save to cookie
   setcookie(
    $cookieName,
    $cookieValue,
@@ -109,9 +127,18 @@ if ($rowCount == 1) {
 
   //retrieve user_id from session
   $user_id = $_SESSION['user_id'];
+
+  /*
+   date is 15days from now
+   ie. current time * 60sec * 60min * 24hrs * 15
+  */
   $expiration = date('Y-m-d H:i:s', time() + 1296000);
 
-  //write query
+  /*
+  save to rememberme table,
+  $authentificator1 is hexadecimal,
+  $authentificator2 is a hashed string
+  */
   $sql = "INSERT INTO rememberme";
   $sql .= "(authentificator1, f2authentificator2, user_id, expires) ";
   $sql .= "VALUES('$authentificator1', '$f2authentificator2', '$user_id', '$expiration')";
