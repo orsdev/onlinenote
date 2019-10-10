224a82ed4e18e0a7b59e72ebda485922da96066d
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-10 06:29:11 +0100

    mainpage.php: unused js script deleted

--- mainpage.php
+++ mainpage.php
@@ -94,7 +94,6 @@ session_start();
  <script src="js/jquery.min.js"></script>
  <script src="js/popper.min.js"></script>
  <script src="js/bootstrap.min.js"></script>
- <script src="js/app.js"></script>
 </body>
 
 </html>
