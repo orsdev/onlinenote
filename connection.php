5399d00b8eabdc829a3e84a1b019082be1d66daf
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-10 16:43:45 +0100

    connection.php: turned off php error from showing to users

--- connection.php
+++ connection.php
@@ -1,10 +1,12 @@
 <?php
+//turn off error reporting for users
+error_reporting(0);
  //connect to database
- $connect = mysqli_connect('localhost' , 'root' , '' , 'onlinenote');
+ $connect = mysqli_connect('localhost' , 'root' , '' , 'onlnenote');
 
  //check if connection is successfull
 if(mysqli_connect_errno()){
- die('<p class="alert alert-danger"> Error: Unable to connect');
+ die('<p class="alert alert-danger"> Error: Unable to connect to database');
 }
 
 ?>
