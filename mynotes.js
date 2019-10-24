bf323ab99cc7f0eb9d86908311d72db3293e5c1f
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-24 06:40:03 +0100

    user can edit note features added

--- mynotes.js
+++ mynotes.js
@@ -2,20 +2,70 @@ $(document).ready(function () {
 
  let activeNote = 0;
 
- //make ajax call
- $.ajax({
-  url: 'loadnotes.php',
-  success: function (response) {
-   $('.notes').html(response);
-  },
-  error: function (error) {
-   $('.alert-container').show();
-   $('#alert-content').text('There was an error with the Ajax Call. Please try again later!');
-  }
+ //load notes function
+ function loadNotes() {
 
- });
+  /*
+    ajax call checks database to see
+    if there are notes and loads it
+   */
+  $.ajax({
+   url: 'loadnotes.php',
+   success: function (response) {
+    $('.notes').html(response);
+   },
+   error: function (error) {
+    $('.alert-container').show();
+    $('#alert-content').text('There was an error with the Ajax Call. Please try again later!');
+   }
 
+  });
+ }
 
+ //function hides and show elements
+ function hideShow(arr1, arr2) {
+  /*
+  loop through array and hide
+  elements
+  */
+  arr1.forEach(value => {
+   $(value).hide();
+  });
+
+  /*
+loop through array and show
+elements
+*/
+  arr2.forEach(value => {
+   $(value).show();
+  });
+
+ }
+
+ //all-note button function
+ function allNote(val1, val2, val3, val4, val5, val6) {
+  //add click event to all note button
+  $('#all-note').click(function () {
+
+   /*
+call hideShow function and
+hide and show targeted elements
+*/
+   hideShow([val1, val2, val3],
+    [val4, val5, val6]
+   );
+
+   //call loadNote function
+   loadNotes();
+
+  });
+
+ }
+
+ //call load note function
+ loadNotes();
+
+ //add-note click event creates note row
  $('#add-note').click(function () {
 
   //make ajax call
@@ -31,79 +81,21 @@ $(document).ready(function () {
      activeNote = response;
 
      //empty textarea
-     $('textarea').empty();
+     $('textarea').val('');
 
      /*
+     call hideShow function and
      hide and show targeted elements
      */
-     $('.notes').hide()
-     //show textarea container container
-     $('.notepad-container').show();
-     //hide add note button
-     $('#add-note').hide();
-     //show all note button
-     $('#all-note').show();
-     //show done button
-     $('#done').show();
-     //hide edit button
-     $('#edit').hide();
+     hideShow(['.notes', '#edit', '#add-note'],
+      ['.notepad-container', '#all-note', '#done']
+     );
 
      //focus textarea
      $('textarea').focus();
 
-     //ajax call to updatenote.php
-     $('textarea').on('keyup', function () {
-      let dataFile = $(this).serializeArray();
-
-      //make ajax call
-      $.ajax({
-       url: 'updatenote.php',
-       type: 'POST',
-       data: { notepad: $('textarea').val(), id: activeNote },
-       success: function (response) {
-
-        if (response == 'error') {
-         $('.alert-container').show();
-         $('#alert-content').text('Unable to update note in the database');
-        }
-       },
-       error: function () {
-        $('#alert-content').text('There was an error with the Ajax Call. Please try again later!');
-       }
-      })
-     })
-
-     //add click event to all-note button
-     $('#all-note').click(function () {
-      //hide target element(all note button)
-      $(this).hide();
-      //show all add note button
-      $('#add-note').show();
-      //hide done button
-      $('#done').hide();
-      //show edit button
-      $('#edit').show();
-      //show notes
-      $('.notes').show();
-      //hide textarea container
-      $('.notepad-container').hide();
-
-
-      //make ajax call
-      $.ajax({
-       url: 'loadnotes.php',
-       success: function (response) {
-        $('.notes').html(response);
-       },
-       error: function (error) {
-        $('.alert-container').show();
-        $('#alert-content').text('There was an error with the Ajax Call. Please try again later!');
-       }
-
-      });
-     });
-
-
+     //call all note function
+     allNote('#all-note', '#done', '.notepad-container', '#add-note', '#edit', '.notes');
     }
    },
    error: function (error) {
@@ -113,6 +105,51 @@ $(document).ready(function () {
   })
  });
 
+ //event to edit and update note
+ $('.notes').on('click', '.mynote', function (e) {
 
+  //store note text in a variable
+  let note = $(this).find('.text').text();
+  /*
+ call hideShow function and
+ hide and show targeted elements
+ */
+  hideShow(['.notes', '#edit', '#add-note'],
+   ['.notepad-container', '#all-note', '#done']
+  );
+
+  //replace textarea  value with clicked note text
+  $('textarea').val(note);
+  //focus textarea
+  $('textarea').focus();
+
+  //change activeNote to id ot note to edit
+  activeNote = $(this).attr('id');
+
+  //call all note function
+  allNote('#all-note', '#done', '.notepad-container', '#add-note', '#edit', '.notes');
+
+ });
+
+ //ajax call to updatenote.php
+ $('textarea').on('keyup', function () {
+
+  //make ajax call
+  $.ajax({
+   url: 'updatenote.php',
+   type: 'POST',
+   data: { mynote: $('textarea').val(), id: activeNote },
+   success: function (response) {
+
+    if (response == 'error') {
+     $('.alert-container').show();
+     $('#alert-content').text('Unable to update note in the database');
+    }
+   },
+   error: function () {
+    $('#alert-content').text('There was an error with the Ajax Call. Please try again later!');
+   }
+  })
+ });
 
 }); 
